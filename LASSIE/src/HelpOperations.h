0c69384a8698c1d681d2c0722384e05b09510b6c
Cindy <xsun63@illinois.edu>
2019-02-04 16:59:31 -0600

    lassie

---
+++ LASSIE/src/HelpOperations.h
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ *
+ *  File Name     : HelpOperations.h
+ *  Date created  : Mar.20 2010
+ *  Authors       : Ming-ching Chiu, Sever Tipei
+ *  Organization  : Music School, University of Illinois at Urbana Champaign
+ *  Description   : This file contains Help Menu Operations used by LASSIE
+ *
+ *==============================================================================
+ *
+ *  This file is part of LASSIE.
+ *  2010 Ming-ching Chiu, Sever Tipei
+ *
+ *
+ *  LASSIE is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LASSIE is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with LASSIE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+#ifndef HELP_OPERATIONS_H
+#define HELP_OPERATIONS_H
+
+#include "LASSIE.h"
+
+namespace HelpOperations{
+  void showAbout();
+  void showContents();
+};
+
+#endif  //end defining namespace HelpOperations
+
