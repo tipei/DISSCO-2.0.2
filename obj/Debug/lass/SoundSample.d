a013f38cea7f8eb47ecccdb574000e82061dfde7
bepzi <dev.ben01189998819991197253@gmail.com>
2019-10-22 22:21:09 -0500

    Remove and blacklist build outputs
    Otherwise, DISSCO cannot be compiled without first running `premake4
    clean`. These files don't need to be in the repo anyways.
    
    Add a `.gitignore` that prevents these build outputs from being
    re-committed to the repo.

--- obj/Debug/lass/SoundSample.d
+++
@@ -1,8 +0,0 @@
-obj/Debug/lass/SoundSample.o: LASS/src/SoundSample.cpp \
- LASS/src/SoundSample.h LASS/src/StandardHeaders.h LASS/src/Types.h
-
-LASS/src/SoundSample.h:
-
-LASS/src/StandardHeaders.h:
-
-LASS/src/Types.h:
