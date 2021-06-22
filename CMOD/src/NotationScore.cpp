8d40016312eea44b7140e9e6a70b234ef5bf19f9
Andrew Orals <nd-0r@andreworals.com>
2021-06-22 16:09:51 -0500

    Implement note insertion NotationScore

--- CMOD/src/NotationScore.cpp
+++ CMOD/src/NotationScore.cpp
@@ -4,25 +4,72 @@ NotationScore::NotationScore(Tempo& tempo) {
   time_signature_ = tempo.getTimeSignature();
   beat_edus_ = stoi(tempo.getEDUPerTimeSignatureBeat().toPrettyString());
   bar_edus_ = stoi(tempo.getEDUPerBar().toPrettyString());
-  unit_note_ = tempo.getTimeSignatureBeat().Den() // the note that represents one beat
+  unit_note_ = tempo.getTimeSignatureBeat().Den(); // the note that represents one beat
 
-  valid_dividers_;
-
-  tuplet_types_;
   tuplet_limit_ = CalculateTupletLimit();
 
   ConstructTupletNames();
 }
 
-NotationScore::CalculateTupletLimit() {
-  size_t i = 1;
+NotationScore::InsertNote(Note* n) {
+  EnsureNoteExpressible(n);
 
-  while (beat_edus_ % i == 0) {
-    valid_divider.push_back(beatEDUs/i);
-    i++;
+  if (n->end_t <= n->start_t ) {
+	  return; // Discard invalid note
   }
 
-  return i;
+  int bar_num = n->start_t / bar_edus_;
+
+  if (bar_num >= notation_score_.size()) {
+    ResizeScore(bar_num);
+  }
+
+  int end = n->end_t / bar_edus_;
+  // if note is completely inside the bar, insert it
+  if(end <= bar_num) {
+    vector<Note*>::iterator iter;
+    for (iter = notation_score_[bar_num]->begin(); 
+         iter != notation_score_[bar_num]->end(); 
+         iter++) {
+      Note* cur = *iter;
+      if (cur->start_t > n->start_t) {
+        notation_score_[bar_num]->insert(iter, n);
+        return;
+      }
+    }
+    notation_score_[bar_num]->insert(iter, n);
+  } else { // split note exceeding barline
+    Note* second = new Note(*n);
+    second->start_t = (bar_num+1) * bar_edus_;
+    n->end_t = (bar_num + 1) * bar_edus_;
+    n->split = 1;
+
+    vector<Note*>::iterator iter;
+    for (iter = notation_score_[bar_num]->begin(); 
+         iter != notation_score_[bar_num]->end(); 
+         iter++) {
+      Note* cur = *iter;
+      if (cur->start_t > n->start_t) {
+        notation_score_[bar_num]->insert(iter, n);
+        InsertNote(second);
+        return;
+      }
+    }
+    
+    notation_score_[bar_num]->insert(iter, n);
+    InsertNote(second);
+  }
+}
+
+NotationScore::CalculateTupletLimit() {
+  size_t tuplet_num = 1;
+
+  while (beat_edus_ % tuplet_num == 0) {
+    valid_dividers_.push_back(beat_edus_ / tuplet_num);
+    tuplet_num++;
+  }
+
+  return tuplet_num;
 }
 
 NotationScore::ConstructTupletNames() {
@@ -31,5 +78,39 @@ NotationScore::ConstructTupletNames() {
     string t = "\\tuplet ";
     t += stoi(i) + "/" + stoi(l) + "{ ";
     tuplet_types_.push_back(t);
+  }
+}
+
+NotationScore::EnsureNoteExpressible(Note* n) {
+  int dur = n->end_t % beat_edus_;
+	int before = n->end_t;
+  int min_diff = beat_edus_;
+  bool note_needs_chop = true;
+
+  for (int i = 0; i < valid_dividers_.size(); i++) {
+    if (dur % valid_divider[i] == 0) {
+      note_needs_chop = false;
+      break;
+    } else {
+      if (min_diff > dur % valid_dividers_[i]) {
+        min_diff = dur % valid_dividers_[i];
+      }
+    }
+  }
+
+  if (note_needs_chop) {
+    n->end_t -= min_diff;
+  }
+}
+
+NotationScore::ResizeScore(size_t new_size) {
+  for(size_t bar_idx = notation_score_.size(); i <= new_size; i++) {
+    vector<Note*>* bar = new vector<Note*>();
+    Note* n = new Note();
+    n->start_t = bar_edus_ * bar_idx;
+    n->end_t = bar_edus_ * bar_idx;
+    n->type_out = " ";
+    bar->push_back(n);
+    notation_score_.push_back(bar);
   }
 }
--- CMOD/src/NotationScore.h
+++ CMOD/src/NotationScore.h
@@ -1,5 +1,5 @@
-#ifndef NotationScore
-#define NotationScore
+#ifndef NOTATION_SCORE_H
+#define NOTATION_SCORE_H
 
 #include "Libraries.h"
 
@@ -13,6 +13,8 @@ typedef vector<vector<Note*>*> Layer;
 /**
  * A class representing a notated score for output using 
  * Lilypond (or something else in the future).
+ * 
+ * Original notation module written by Haorong Sun
 **/
 class NotationScore {
 
@@ -24,9 +26,54 @@ public:
   **/
   NotationScore(Tempo& tempo);
 
-  void insert_note(Note* note);
+  // TODO - implement copy constructor
+  // TODO - implement move constructor
+  // TODO - implement assignment operator
+  // TODO - implement destructor
+
+  /**
+   * Insert a note into this score.
+   * 
+   * @param n A pointer to the note to insert
+  **/
+  void InsertNote(Note* n);
+  
+  /**
+   * Build the text representation of this score.
+  **/
+  void BuildScore();
+
+  /**
+   * Output the text representation of this score.
+   * 
+   * @param out_stream The stream to which the text will be appended
+   * @returns The appended stream
+  **/
+  ostream& operator>>(ostream& out_stream) const;
 
 private:
+  size_t CalculateTupletLimit();
+
+  void ConstructTupletNames();
+
+  /**
+   * If the remainder given by the note's end time modulo the edu's per beat
+   * is not divisible by any element of valid_dividers_, find the smallest 
+   * difference between a multiple of the valid dividers and the remainder. 
+   * Then, shorten the note's duration by this difference to make the note's
+   * duration expressible.
+   * 
+   * @param n A pointer to the note whose duration to examine and modify
+  **/
+  void EnsureNoteExpressible(Note* n);
+
+  /**
+   * Resize the score to the specified size.
+   * 
+   * @param new_size The desired size of the score
+  **/
+  void ResizeScore(size_t new_size);
+
   string time_signature_; // NOTE - only need this for outputting the actual score
   int beat_edus_;
   int bar_edus_;
@@ -38,15 +85,11 @@ private:
   int tuplet_limit_; // the greatest allowed tuplet type (exclusive)
 
   Layer notation_score_;
+  stringstream score_;
 
   //a integer variable to indicate the previous tuplet type in notation loop
   static int pre_tuplet = 0;
   static string loudness_prev = "";
-
-  size_t CalculateTupletLimit();
-
-  void ConstructTupletNames();
-
 };
 
 #endif
