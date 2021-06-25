0c3a6f0e74e6f5ab40f438e16984a316fe007f00
Andrew Orals <nd-0r@andreworals.com>
2021-06-24 22:45:51 -0500

    Fix unitialized variable bug

--- CMOD/src/Event.h
+++ CMOD/src/Event.h
@@ -148,7 +148,7 @@ protected:
     double checkPoint;
 
    //Previous start time for Sweep             //added by sever
-    double previousChildStartTime;
+    double previousChildStartTime = 0.0f;
 
     //Previous start time (only used by sweep generation method)
     TimeSpan tsPrevious;
